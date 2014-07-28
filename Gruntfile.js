ee44bbc1bc0d95a061ffc67ef3af410eb7895436
Benjamin Landers <benjaminrlanders@gmail.com>
2014-07-28 13:49:31 -0700

    added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = function(grunt) {
+  var sources = 'src/**/*.js';
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    nodeunit: {
+      files: ['test/**/*_test.js'],
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: [sources]
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+    },
+    uglify: {
+       options: {
+         banner: '<%= banner %>'
+       },
+       dist: {
+         src: '<%= browserify.dev.dest %>',
+         dest: '<%= pkg.name %>.min.js'
+       }
+     },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
+    },
+    browserify:{
+      dev: {
+        src: [sources],
+        dest: '<%= pkg.name %>.js',
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  // Default task.
+  grunt.registerTask('default', ['test', 'hint','build']);
+  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('hint', ['jshint']);
+  grunt.registerTask('build', ['browserify','uglify']);
+};
