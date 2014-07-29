3151731b80f5ae59d2dd68559f2c7dc284960888
Benjamin Landers <benjaminrlanders@gmail.com>
2014-07-29 13:54:56 -0700

    add gen method and improve instruction list

--- src/compiler.js
+++ src/compiler.js
@@ -2,8 +2,16 @@ var stdlib = require('./instructions.js');
 
 exports.compile = function(lines){
   var context = {};
-  var out = 'var heap = new Int8Array(size),\n\
-  registers = new Int32Array(16),\n\
+  var out = 'var heap = new ArrayBuffer(size),\n\
+  ui8 = new Uint8Array(heap),\n\
+  ui16= new Uint16Array(heap),\n\
+  ui32= new Uint32Array(heap),\n\
+  i8 = new Int8Array(heap),\n\
+  i16= new Int16Array(heap),\n\
+  i32= new Int32Array(heap),\n\
+  registers = new ArrayBuffer(16),\n\
+  ureg = new Uint32Buffer(registers),\n\
+  reg = new Int32Buffer(registers),\n\
   N = 0, C = 0, Z = 0, V = 0;\n\
   switch(registers[15]){\n';
   return out;
@@ -26,7 +34,7 @@ exports.codegen = function(line,number,context){
   if(line.option){
     template = instruction.option[line.option].replace('S',instruction.template);
   }
-  out += exports.generate(template,context) + ';';
+  out += exports.generate(template,line.args,context) + ';';
   if(line.conditional){
     out += '}';
   }
@@ -36,6 +44,20 @@ exports.codegen = function(line,number,context){
   return out;
 };
 
-exports.generate = function(template,context){
+exports.generate = function(template,args,context){
+  var input = [];
+  for(var i = 0;i < args.length;i++){
+    if(args[i] instanceof Array){
+      args[i] = args[i][0]; //temp until I implement barrel codegen
+    }
+    if(args[i].startsWith('#')){
+      input[i] = args[i].slice(1);
+    }
+    if(stdlib.registers.indexOf(args[i]) >= 0){
+      input[i] = "ureg["+(stdlib.registers.indexOf(args[i])%16)+']';
+    }
+    template = template.replace('$'+i,input[i]);
+  }
+  template = template.replace('$'+(args.length+1),'0');
   return template;
 };
